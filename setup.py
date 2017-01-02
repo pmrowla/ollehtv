110c927835d5814e170f8296abb7111ae5a7c2a9
Peter Rowlands <peter@pmrowla.com>
2017-01-02 18:28:32 +0900

    add packaging and travis junk

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='ollehtv',
+    version='0.1.0',
+    description='Python library for controlling an Olleh TV STB',
+    long_description=long_description,
+    url='https://github.com/pmrowla/ollehtv',
+    author='Peter Rowlands',
+    author_email='peter@pmrowla.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Multimedia',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='olleh ollehtv',
+    py_modules=['ollehtv'],
+    install_requires=['requests'],
+)

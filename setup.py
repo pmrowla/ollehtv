d7867933ca454f7d52166908c2def39856e78c9c
Peter Rowlands <peter@pmrowla.com>
2017-01-03 18:33:11 +0900

    increment version
    [skip ci]

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='ollehtv',
-    version='0.1.0',
+    version='0.2.0-git',
     description='Python library for controlling an Olleh TV STB',
     long_description=long_description,
     url='https://github.com/pmrowla/ollehtv',

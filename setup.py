26c93c2b77b8e50ae2b7368c715af1ef156cc356
Peter Rowlands <peter@pmrowla.com>
2017-01-04 21:18:36 +0900

    release 0.1.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='ollehtv',
-    version='0.1.1',
+    version='0.1.2',
     description='Python library for controlling an Olleh TV STB',
     long_description=long_description,
     url='https://github.com/pmrowla/ollehtv',
